631acbdd0c10cf54071e2db1f9e968c303a4c44d
Adriano Santos <sleipnir@bsd.com.br>
2021-07-12 20:54:12 -0300

    Defining types

--- lib/rsocketx.ex
+++ lib/rsocketx.ex
@@ -1,2 +1,23 @@
-defmodule Rsocketx do
+defmodule RSocketx do
+  @moduledoc """
+  RSocketx
+  """
+
+  defmodule Payload do
+    defstruct [
+      :has_metadata?,
+      :metadata,
+      :data,
+      :metadata_utf8,
+      :data_utf8
+    ]
+
+    @type t() :: %__MODULE__{
+            has_metadata?: boolean(),
+            metadata: binary() | nil,
+            data: binary(),
+            metadata_utf8: String.t(),
+            data_utf8: String.t()
+          }
+  end
 end
--- lib/rsocketx/rsocket_acceptor.ex
+++ lib/rsocketx/rsocket_acceptor.ex
@@ -2,14 +2,19 @@ defmodule RSocketx.RSocketAcceptor do
   @moduledoc """
   RSocket Acceptor.
   """
+  alias RSocketx.Payload
 
-  @callback handle_fire_and_forget(any()) :: any()
+  @type payload :: Payload
 
-  @callback handle_request_response(any()) :: any()
+  @type stream_payload :: Stream.t()
 
-  @callback handle_request_stream(any()) :: any()
+  @callback handle_fire_and_forget(payload()) :: any()
 
-  @callback handle_request_channel(any()) :: any()
+  @callback handle_request_response(payload()) :: payload()
+
+  @callback handle_request_stream(payload()) :: stream_payload()
+
+  @callback handle_request_channel(stream_payload()) :: stream_payload()
 
   @optional_callbacks handle_fire_and_forget: 1,
                       handle_request_response: 1,
@@ -27,16 +32,16 @@ defmodule RSocketx.RSocketAcceptor do
 
   defmacro __before_compile__(_env) do
     quote do
-      @spec handle_fire_and_forget(any()) :: any()
+      @spec handle_fire_and_forget(payload()) :: any()
       def handle_fire_and_forget(_), do: Logger.debug("Received an fire and forget message")
 
-      @spec handle_request_response(any()) :: any()
+      @spec handle_request_response(payload()) :: payload()
       def handle_request_response(_), do: Logger.debug("Received an request response message")
 
-      @spec handle_request_stream(any()) :: any()
+      @spec handle_request_stream(payload()) :: stream_payload()
       def handle_request_stream(_), do: Logger.debug("Received an request stream message")
 
-      @spec handle_request_channel(any()) :: any()
+      @spec handle_request_channel(stream_payload()) :: stream_payload()
       def handle_request_channel(_), do: Logger.debug("Received an request channel message")
     end
   end
